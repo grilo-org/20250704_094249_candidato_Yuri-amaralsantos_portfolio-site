b01333b7adda3376f18ffcae50a7b7e6b42922d2
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-03-15 12:02:43 -0300

    tirando rotas adicionais

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../src/components/pages/home";
-import Porfolio from "../src/components/pages/portfolio";
 
 import Nav from "../src/components/elements/nav/Nav";
 import Footer from "../src/components/elements/footer/Footer";
@@ -13,7 +12,6 @@ const AppRouter = () => {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Porfolio />} />
       </Routes>
       <Footer />
     </Router>
