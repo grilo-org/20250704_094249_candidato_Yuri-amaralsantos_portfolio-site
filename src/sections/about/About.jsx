e1710a843e59e65384e04a1a2de15fc923c31844
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-06-23 16:54:37 -0300

    consertando página de texto

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./About.css";
 
 const About = () => {
@@ -9,23 +8,15 @@ const About = () => {
         <p>
           Olá, meu nome é Yuri Amaral Santos, sou natural do Rio de Janeiro e me
           formei em Análise e Desenvolvimento de Sistemas pela Universidade
-          Estácio de Sá (UNESA).
-        </p>
-        <p>
-          Sou desenvolvedor e designer, com grande paixão por criar soluções
-          inovadoras e funcionais. Minha principal ferramenta de design é o
-          Figma.
-        </p>
-        <p>
-          No desenvolvimento, sou especializado em JavaScript, Python e java, e
-          utilizo os frameworks Vue.js e React para front-end. Tenho experiência
-          em desenvolvimento de aplicativos móveis com Dart e Flutter.
-        </p>
-        <p>
-          Além disso, possuo experiência na criação de jogos com Unity, no
-          desenvolvimento de bancos de dados e no gerenciamento de serviços de
-          nuvem, sempre buscando aprimorar minhas habilidades e entregar
-          resultados de qualidade.
+          Estácio de Sá (UNESA). Sou desenvolvedor e designer, com grande paixão
+          por criar soluções inovadoras e funcionais. Minha principal ferramenta
+          de design é o Figma. No desenvolvimento, sou especializado em
+          JavaScript, Python e java, e utilizo os frameworks Vue.js e React para
+          front-end. Tenho experiência em desenvolvimento de aplicativos móveis
+          com Dart e Flutter. Além disso, possuo experiência na criação de jogos
+          com Unity, no desenvolvimento de bancos de dados e no gerenciamento de
+          serviços de nuvem, sempre buscando aprimorar minhas habilidades e
+          entregar resultados de qualidade.
         </p>
       </div>
       <div className="experiencia">
